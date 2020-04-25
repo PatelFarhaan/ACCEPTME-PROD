0be0309ebdf44a3807e8e1f99b0f05f8b7d70c0f
Ubuntu <ubuntu@ip-172-31-38-93.ap-south-1.compute.internal>
2020-04-25 01:14:01 +0000

    v2

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from project import app, db
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True, use_reloader=True, host='0.0.0.0', threaded=True, port=8000)
+    app.run(debug=True, use_reloader=True, host='0.0.0.0', threaded=True, port=80)
